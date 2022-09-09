4b4cbfd1614845148b1eee406ea654e4a40e6bab
aeon <jerry73204@gmail.com>
2022-09-09 12:56:08 +0800

    Keep temporary files during makepkg

--- run.sh
+++ run.sh
@@ -2,27 +2,23 @@
 
 set -e
 
+# Save and change the owner of /build
+orig_owner=$(stat -c '%u:%g' /build)
+sudo chown -R $(id -u):$(id -g) /build
+
 # Make a copy so we never alter the original
-cp -r /pkg /tmp/pkg
-cd /tmp/pkg
+rsync -a --delete /pkg/ /build
+cd /build
 
 # Sync database
-if [ -n "$SYNC_DATABASE" ]; then
-    paru -S --refresh
-fi
+paru -S --refresh
 
 # Do the actual building. Paru will fetch all dependencies for us (including
 # AUR dependencies) and then build the package.
 paru -U --noconfirm
 
-# Store the built package(s). Ensure permissions match the original PKGBUILD.
-if [ -n "$EXPORT_PKG" ]; then
-    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./*pkg.tar*
-    sudo mv ./*pkg.tar* /pkg
-fi
 # Export .SRCINFO for built package
-if [ -n "$EXPORT_SRC" ]; then
-    makepkg --printsrcinfo > .SRCINFO
-    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./.SRCINFO
-    sudo mv ./.SRCINFO /pkg
-fi
+makepkg --printsrcinfo > .SRCINFO
+
+# Restore the owner of /build
+chown -R $orig_owner /build
